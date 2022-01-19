7b1c61c03a56865e3f74fd988c93e5fe1b0a2f36
Scott Markley <69651158+snmarkley1@users.noreply.github.com>
2022-01-19 16:21:50 -0500

    Add files via upload

--- scripts/07_pull_in_airport_data.R
+++ scripts/07_pull_in_airport_data.R
@@ -11,32 +11,77 @@
 source("scripts/00_preamble.R")
 
 ## Check Workspace
-getwd()  # D:/HHUUD10
+getwd()  # D:/HIST_HU_URB
 
 ##################################################################
 ##  STEP 1: LOAD AIRPORT DATA                                   ##
 ##################################################################
 
-## Import data from FAA (Date: June 28, 2021)
-airports_import <- read.delim(
-  "https://www.faa.gov/airports/airport_safety/airportdata_5010/menu/nfdcfacilitiesexport.cfm?Region=&District=&State=&County=&City=&Use=&Certification=",
-  header = TRUE,
-  sep = "\t") %>%
-  as_tibble(.) %>%
+## !! FAA link from original has BROKEN !!
+## To replicate results in HHUUD10, use "airports_yr.dbf" file in "tables" folder
+## To produce new results with updated FAA data, skip to Line 66
+
+
+# Original import (Date: June 28, 2021)
+#airports_import <- read.delim(
+#  "https://www.faa.gov/airports/airport_safety/airportdata_5010/menu/nfdcfacilitiesexport.cfm?Region=&District=&State=&County=&City=&Use=&Certification=",
+#  header = TRUE,
+#  sep = "\t") %>%
+#  as_tibble(.) %>%
+#  print()
+
+
+## Organize for Export into ArcGIS Pro
+#airports <- airports_import %>%
+  # # include only airports in Lower 48 + DC
+  # filter(!State %in% c("AK", "AS", "GU","HI", "MP", "PR", "QM", "QW", "VI", "XL")) %>%
+  # # filter out heliports, gliderports, etc
+  # filter(Type == "AIRPORT") %>%
+  # # keep only relevant columns
+  # select(LocationID, State, ActivationDate) %>%
+  # rename(
+  #   LOC_ID = 1,  # match Esri airport attribute name
+  #   STATE = 2,  # keep the state
+  #   ACT_DATE = 3  # date in which the airport was added to the NFDC airport database (https://www.faa.gov/airports/airport_safety/airportdata_5010/media/airport-data-dictionaries.xlsx)
+  # ) %>%
+  # # fix up columns
+  # mutate(
+  #   LOC_ID = str_extract(LOC_ID, "\\w+"),  # keep only alphabetic letters (removes apostrophe)
+  #   ACT_DATE = as.integer(str_extract(ACT_DATE, "\\d+$"))  # keep only ending digits (keeps years, removes day & month)
+  # ) %>%
+  # print() # n = 12,539
+  
+
+## Save out to tables folder
+#write.dbf(as.data.frame(airports), "tables/airports_yr.dbf")
+
+
+## Inspect original file output
+airports_yr <- read.dbf("tables/airports_yr.dbf") %>%
+  as_tibble() %>%
   print()
 
-# Save locally
-#write_csv(airports_import, "tables/airports_raw.csv") 
+
+##------------------------------------------------------------------------------------------
+##------------------------------------------------------------------------------------------
+## Import newer data from FAA: pre-downloaded in "tables" folder (Date: Jan. 19, 2022)
+##------------------------------------------------------------------------------------------
+##------------------------------------------------------------------------------------------
+
+
+## Import table
+airports_import <- read_csv("tables/faa_airport_data.csv") %>%
+  print()
 
 
 ## Organize for Export into ArcGIS Pro
 airports <- airports_import %>%
   # include only airports in Lower 48 + DC
-  filter(!State %in% c("AK", "AS", "GU","HI", "MP", "PR", "QM", "QW", "VI", "XL")) %>%
+  filter(!`State Id` %in% c("AK", "AS", "GU","HI", "MP", "PR", "QM", "QW", "VI", "XL")) %>%
   # filter out heliports, gliderports, etc
-  filter(Type == "AIRPORT") %>%
+  filter(`Facility Type` == "AIRPORT") %>%
   # keep only relevant columns
-  select(LocationID, State, ActivationDate) %>%
+  select(`Loc Id`, `State Id`, `Activation Date`) %>%
   rename(
     LOC_ID = 1,  # match Esri airport attribute name
     STATE = 2,  # keep the state
@@ -45,16 +90,12 @@ airports <- airports_import %>%
   # fix up columns
   mutate(
     LOC_ID = str_extract(LOC_ID, "\\w+"),  # keep only alphabetic letters (removes apostrophe)
-    ACT_DATE = as.integer(str_extract(ACT_DATE, "\\d+$"))  # keep only ending digits (keeps years, removes day & month)
+    ACT_DATE = as.integer(str_extract(ACT_DATE, "\\d+"))  # keep only ending digits (keeps years, removes day & month)
   ) %>%
   print() # n = 12,539
-  
-
-## Save out to tables folder
-write.dbf(as.data.frame(airports), "tables/airports_yr.dbf")
 
 
-
-
+## overwrite existing file
+#write.dbf(as.data.frame(airports), "tables/airports_yr.dbf")
 
 
