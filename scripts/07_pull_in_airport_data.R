f251c30607dd28bdf90f8c39634ae210e2485de3
Scott Markley <69651158+snmarkley1@users.noreply.github.com>
2022-01-19 16:41:47 -0500

    Add files via upload

--- scripts/07_pull_in_airport_data.R
+++ scripts/07_pull_in_airport_data.R
@@ -68,31 +68,36 @@ airports_yr <- read.dbf("tables/airports_yr.dbf") %>%
 ##------------------------------------------------------------------------------------------
 ##------------------------------------------------------------------------------------------
 
+## INSTRUCTIONS: If you want to use the most up-to-date FAA file available, you will need to 
+## download it from the FAA site here: https://adip.faa.gov/agis/public/#/airportSearch/advanced.
+## Next, you will need to save it locally as a csv file in your tables folder under the name
+## "faa_airport_data_new.csv".
 
-## Import table
-airports_import <- read_csv("tables/faa_airport_data_new.csv") %>%
-  print()
 
-
-## Organize for Export into ArcGIS Pro
-airports <- airports_import %>%
-  # include only airports in Lower 48 + DC
-  filter(!`State Id` %in% c("AK", "AS", "GU","HI", "MP", "PR", "QM", "QW", "VI", "XL")) %>%
-  # filter out heliports, gliderports, etc
-  filter(`Facility Type` == "AIRPORT") %>%
-  # keep only relevant columns
-  select(`Loc Id`, `State Id`, `Activation Date`) %>%
-  rename(
-    LOC_ID = 1,  # match Esri airport attribute name
-    STATE = 2,  # keep the state
-    ACT_DATE = 3  # date in which the airport was added to the NFDC airport database (https://www.faa.gov/airports/airport_safety/airportdata_5010/media/airport-data-dictionaries.xlsx)
-  ) %>%
-  # fix up columns
-  mutate(
-    LOC_ID = str_extract(LOC_ID, "\\w+"),  # keep only alphabetic letters (removes apostrophe)
-    ACT_DATE = as.integer(str_extract(ACT_DATE, "\\d+"))  # keep only ending digits (keeps years, removes day & month)
-  ) %>%
-  print() # n = 12,539
+### Import table
+# airports_import <- read_csv("tables/faa_airport_data_new.csv") %>%
+#   print()
+# 
+# 
+# ## Organize for Export into ArcGIS Pro
+# airports <- airports_import %>%
+#   # include only airports in Lower 48 + DC
+#   filter(!`State Id` %in% c("AK", "AS", "GU","HI", "MP", "PR", "QM", "QW", "VI", "XL")) %>%
+#   # filter out heliports, gliderports, etc
+#   filter(`Facility Type` == "AIRPORT") %>%
+#   # keep only relevant columns
+#   select(`Loc Id`, `State Id`, `Activation Date`) %>%
+#   rename(
+#     LOC_ID = 1,  # match Esri airport attribute name
+#     STATE = 2,  # keep the state
+#     ACT_DATE = 3  # date in which the airport was added to the NFDC airport database (https://www.faa.gov/airports/airport_safety/airportdata_5010/media/airport-data-dictionaries.xlsx)
+#   ) %>%
+#   # fix up columns
+#   mutate(
+#     LOC_ID = str_extract(LOC_ID, "\\w+"),  # keep only alphabetic letters (removes apostrophe)
+#     ACT_DATE = as.integer(str_extract(ACT_DATE, "\\d+"))  # keep only ending digits (keeps years, removes day & month)
+#   ) %>%
+#   print() # n = 12,539
 
 
 ## overwrite existing file
